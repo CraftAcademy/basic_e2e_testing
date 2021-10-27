23a601f8ce60d55cc945f0ac595fba49b761634a
Thomas <thomas@craftacademy.se>
2021-10-27 13:59:39 +0200

    adds demo code for simple registration process

--- cypress/integration/userCanSeeAWelcomeMessage.feature.js
+++ cypress/integration/userCanSeeAWelcomeMessage.feature.js
@@ -1,12 +1,17 @@
 describe("user that navigates to the application", () => {
-
-
+  // Step 2 INTERACT / ACT
   before(()=> {
     cy.visit('/')
   })
 
+  // Step 3 ASSERT
   it('is expected to see "Hello World"', ()=> {
     cy.get('h1').should('contain.text', 'Hello mars')
     cy.get('h3').should('contain.text', 'My first test')
   })
 });
+
+
+// SETUP - INTERACT - ASSERT
+
+// ARRANGE - ACT  ASSERT
